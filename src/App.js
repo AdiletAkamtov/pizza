f63818da00ee48fb7f284312142ac9301d0a3a2d
AdiletAkamtov <adiletakmatovofficial@gmail.com>
2022-11-23 04:32:07 +0600

    feat: add new section cart and mobile first

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
 import './scss/app.scss'
 import Header from './components/Header'
-import Categories from './components/Categories'
-import Sort from './components/Sort'
-import PizzaBlock from './components/PizzaBlock'
-
-import pizzas from './assets/pizza.json'
+import Home from './pages/Home'
+import Cart from './pages/Cart'
+import NotFound from './pages/NotFound'
 
 function App() {
 	return (
-		<div classNameName='App'>
-			<div className='wrapper'>
-				<Header />
-				<div className='content'>
-					<div className='container'>
-						<div className='content__top'>
-							<Categories />
-							<Sort />
-						</div>
-						<h2 className='content__title'>Все пиццы</h2>
-						<div className='content__items'>
-							{pizzas.map(obj => (
-								<PizzaBlock key={obj.id} {...obj} />
-							))}
-						</div>
-					</div>
+		<div className='wrapper'>
+			<Header />
+			<div className='content'>
+				<div className='container'>
+					<Routes>
+						<Route path='/' element={<Home />} />
+						<Route path='/cart' element={<Cart />} />
+						<Route path='*' element={<NotFound />} />
+					</Routes>
 				</div>
 			</div>
 		</div>
---
+++ src/components/NotFoundBlock/NotFoundBlock.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './NotFoundBlock.module.scss'
+
+const NotFound = () => {
+	return <h1 className={styles.root}>Нечего не найдно</h1>
+}
+
+export default NotFound
